c23324b75f72796ac62486ae01a5480342e4e451
Alex Sheshnev <82956605+ithr-alex@users.noreply.github.com>
2025-09-18 07:32:33 +0300

    mistakes fixed

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -70,7 +70,7 @@ const Index = () => {
               onClick={() => navigate('/builder')}
               className="text-lg px-8 py-6 bg-primary hover:bg-primary-dark"
             >
-              Start CV Building Now
+              Start Building CV Now
               <ArrowRight className="ml-2 h-5 w-5" />
             </Button>
           </div>
