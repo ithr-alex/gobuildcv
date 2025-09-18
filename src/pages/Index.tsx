d8b841c877386a29a25392090c8f4ba667afba5d
Alex Sheshnev <82956605+ithr-alex@users.noreply.github.com>
2025-09-18 12:20:27 +0300

    font

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -44,7 +44,7 @@ const Index = () => {
               <FileText className="h-8 w-8 text-primary" />
               <span className="text-xl font-bold text-foreground">GoBuildCV</span>
               <span className="text-xs font-regular text-foreground">by</span>
-              <a href="https://GoTalent.agency" target="_blank" rel="noopener noreferrer" className="text-l font-regular text-foreground">
+              <a href="https://GoTalent.agency" target="_blank" rel="noopener noreferrer" className="text-xs font-regular text-foreground">
   GoTalent.agency
 </a>
             </div>
